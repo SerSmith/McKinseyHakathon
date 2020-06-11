ef8cce4f2738f2001ded98c17c59348ae17bfe96
Marina_S <Mila_601@mail.ru>
2020-06-11 21:11:08 +0300

    added function for splitting in train and val

--- model.py
+++ model.py
@@ -3,34 +3,43 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from scipy import stats
 import optimisation
+import random
 
+def split_train_val(df_train, df_test, percent_train):
+    # Функция, которая разбивает выборку на трейн и валидацию (которая начаниется с минимального года из теста)
+    date_test_min = min(df_test.galacticyear)
+    k = int(df_train.shape[0] * percent_train)
+    df_train_new = df_train.loc[random.sample(list(df_train.index),k)]
+    df_val_new = df_train[df_train.galacticyear >= date_test_min]
+    return df_train_new.drop('y',axis=1), df_val_new.drop('y',axis=1), df_train_new['y'], df_val_new['y']
+ 
 
-
-def train_model(df_train, trend_y = True, percent_val=0.2, y_model=None, previous_residuals=None, round_digits=None):
-    X_train, X_test, y_train, y_test = train_test_split(df_train.drop('y', axis=1), df_train['y'], test_size=percent_val)
+def train_model(df_train, df_test, trend_y = True, percent_train=0.8, y_model=None, previous_residuals=None, round_digits=None):
+    X_train, X_val, y_train, y_val = split_train_val(df_train, df_test, percent_train)
     gbm = lgb.LGBMRegressor(objective='rmse', max_depth=5, num_leaves=26, learning_rate=0.060, colsample_bytree=0.800,
                             subsample=0.968, n_estimators=451)
+    print(X_train.shape, X_val.shape, min(X_val.galacticyear))
     if trend_y:
         columns=['y_trend','galaxy']
     else:
         columns=['galaxy']
     gbm.fit(X_train.drop(columns=columns), y_train,
-        eval_set=[(X_test.drop(columns=columns), y_test)],
+        eval_set=[(X_val.drop(columns=columns), y_val)],
         eval_metric='RMSE')
-    predict = gbm.predict(X_test.drop(columns=columns))
+    predict = gbm.predict(X_val.drop(columns=columns))
 
     
     if trend_y:
-        predict += X_test['y_trend']
+        predict += X_val['y_trend']
         y_train += X_train['y_trend']
-        y_test += X_test['y_trend']
+        y_val += X_val['y_trend']
 
 
     rank_diviation = None
 
     if previous_residuals is not None and y_model is not None and round_digits is not None:
 
-        possibal_points = [np.array(previous_residuals) + res for res in y_test]
+        possibal_points = [np.array(previous_residuals) + res for res in y_val]
 
         kernels_all = []
 
@@ -39,7 +48,7 @@ def train_model(df_train, trend_y = True, percent_val=0.2, y_model=None, previou
             kernels_all += [kernel]
 
 
-        y_prob_model = np.zeros([y_test.shape[0], y_model.shape[0]])
+        y_prob_model = np.zeros([y_val.shape[0], y_model.shape[0]])
 
         for i, kernel in enumerate(kernels_all):
             y_prob_model[i, :] = kernel(y_model)
@@ -52,16 +61,16 @@ def train_model(df_train, trend_y = True, percent_val=0.2, y_model=None, previou
         
         probs_rank = probs.sum(axis=0).argsort()/probs.shape[0]
 
-        y_test_rank = y_test.argsort()/probs.shape[0]
+        y_val_rank = y_val.argsort()/probs.shape[0]
         
-        rank_diviation = np.sum((probs_rank - y_test_rank) ** 2)/y_test_rank.shape[0]
+        rank_diviation = np.sum((probs_rank - y_val_rank) ** 2)/y_val_rank.shape[0]
 
 
 
-    return X_train, X_test, y_train, y_test, gbm, predict, rank_diviation
+    return X_train, X_val, y_train, y_val, gbm, predict, rank_diviation
 
 
-def run_model_and_distrs(train, test, trend_y=True, percent_val=0.2, qunity_starts=1, quantity_points_out=100, edges_percent=0.2, round_digits=6):
+def run_model_and_distrs(train, test, trend_y=True, percent_train=0.8, qunity_starts=1, quantity_points_out=100, edges_percent=0.2, round_digits=6):
 
     previous_residuals = None
     residuals_all = []
@@ -73,7 +82,7 @@ def run_model_and_distrs(train, test, trend_y=True, percent_val=0.2, qunity_star
 
 
     for i in range(qunity_starts):
-        X_train, X_validate, y_train, y_validate, model_out, predict, rank_diviation  = train_model(train, trend_y, percent_val=percent_val, y_model=y_model, previous_residuals=previous_residuals, round_digits=round_digits)
+        X_train, X_validate, y_train, y_validate, model_out, predict, rank_diviation  = train_model(train, test, trend_y, percent_train=percent_train, y_model=y_model, previous_residuals=previous_residuals, round_digits=round_digits)
 
         residuals = [y - y_true for y, y_true in zip(list(y_validate), predict)]
         previous_residuals = residuals
